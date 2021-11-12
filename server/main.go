5ecf161dfc4efe736732333c2c7972d8e09b8f7d
lemon-mint <ice.lemons.mint@gmail.com>
2021-11-12 22:57:36 +0900

    test server

--- server/main.go
+++ server/main.go
@@ -1,5 +1,17 @@
-package server
+package main
+
+import (
+	"net"
+
+	"github.com/lemon-mint/lemonmq"
+)
 
 func main() {
-
+	server := lemonmq.NewServer(0)
+	ln, err := net.Listen("tcp", ":5555")
+	if err != nil {
+		panic(err)
+	}
+	server.Ln = ln
+	server.Serve()
 }
